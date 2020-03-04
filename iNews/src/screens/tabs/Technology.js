c171dc53fcae7ce05c4d91d6f5cdbc7bcb875916
kasra <kniktash@gmail.com>
2020-03-03 17:16:00 -0800

    fill each tab with mock list + thumbnail

--- iNews/src/screens/tabs/Technology.js
+++ iNews/src/screens/tabs/Technology.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button
+} from "native-base";
 
-export default class Technology extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
+export default class Science extends Component {
   render() {
     return (
-      <View>
-        <Text> Technology </Text>
-      </View>
+      <Container>
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail
+                  square
+                  source={{
+                    uri:
+                      "https://www.euractiv.com/wp-content/uploads/sites/2/2014/03/news-default.jpeg"
+                  }}
+                />
+              </Left>
+              <Body>
+                <Text>Technology Article</Text>
+                <Text note numberOfLines={2}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Nostrum qui natus saepe. Obcaecati culpa id qui corrupti
+                  delectus ullam tempora earum perferendis repudiandae sed,
+                  optio facere omnis consectetur illo aliquam.
+                </Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
